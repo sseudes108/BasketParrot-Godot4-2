c09a5e6418efbe54752de96ab95bc8d3042126ae
Senstur Games <senstur.art@gmail.com>
2023-09-23 13:21:14 -0300

    0.3

--- Project/Animal/Parrot/Parrot.gd
+++ Project/Animal/Parrot/Parrot.gd
@@ -2,6 +2,15 @@ extends RigidBody2D
 
 var dead: bool = false
 
+var _dragging: bool = false
+var _released: bool = false
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+var _last_dragged_position: Vector2 = Vector2.ZERO
+var _last_drag_amount: float = 0.0
+var _fired_time: float = 0.0
+
 func _ready():
 	pass 
 
@@ -30,3 +39,8 @@ func ExitedScreen():
 	dead = true
 	SignalManager.ParrotDead.emit()
 	pass
+
+
+func MouseInput(viewport, event: InputEvent, shape_idx):
+	if event.is_action_pressed("Drag"):
+		print(event)
